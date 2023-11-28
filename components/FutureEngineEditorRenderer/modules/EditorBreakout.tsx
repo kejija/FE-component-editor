e372501b61290cc07abd39a56cfdf123ec1608fa
Ji mac mini <kejija@hotmail.com>
2023-11-27 22:43:36 -0500

    minor key bug

--- components/FutureEngineEditorRenderer/modules/EditorBreakout.tsx
+++ components/FutureEngineEditorRenderer/modules/EditorBreakout.tsx
@@ -20,130 +20,128 @@ import DataPlotter from './DataPlotter';
 export function Breakout(props) {
   const { data, height, mode, index, parameter_type, componentID } = props;
   const data_types = ['number', 'table', 'string', 'boolean', 'option', 'datasheet'];
-  const additional_controls = [];
+  // let additional_controls = null;
 
   const { deleteParameter, updateParameter } = useStore();
-
-  switch (data.data_type) {
-    case 'number':
-      additional_controls.push(
-        <>
-          <TextInput
-            key="number-default-value-editor"
-            size="xs"
-            value={data.default_value}
-            onChange={(e) =>
-              updateParameter(componentID, parameter_type, index, {
-                default_value: e.target.value,
-              })
-            }
-            withAsterisk
-            w={100}
-            description="Default Value"
-          />
-          <Select
-            key="select-default-unit-editor"
-            w={60}
-            description="units"
-            size="xs"
-            value={data.default_unit}
-            onChange={(e) => updateParameter(componentID, parameter_type, index, { default_unit: e })}
-            data={['mm']}
-          />
-        </>
-      );
-      break;
-    case 'table':
-      additional_controls.push(
-        <>
-          <Popover
-            key="table-editor"
-            closeOnClickOutside={false}
-            trapFocus
-            width={500}
-            position="bottom"
-            withArrow
-            shadow="md"
-          >
-            <Popover.Target>
-              <Button size="xs" variant="outline">
-                Edit Table Data
-              </Button>
-            </Popover.Target>
-            <Popover.Dropdown>
-              <SvelteJSONEditor
-                mode="table"
-                content={{ json: data.table }}
-                onChange={(e) =>
-                  updateParameter(componentID, parameter_type, index, { table: e.json })
-                }
-              />
-              <Center>
-                <DataPlotter title={data.label} data={data.table} />
-              </Center>
-            </Popover.Dropdown>
-          </Popover>
-        </>
-      );
-      break;
-    case 'string':
-      break;
-    case 'boolean':
-      break;
-    case 'option':
-      additional_controls.push(
-        <>
-          <Popover
-            trapFocus
-            width={500}
-            position="bottom"
-            withArrow
-            shadow="md"
-          >
-            <Popover.Target>
-              <Button size="xs" variant="outline">
-                Edit Options
-              </Button>
-            </Popover.Target>
-            <Popover.Dropdown>
-              <TagsInput
-                variant="filled"
-                size="xs"
-                description="options list"
-                value={data.options || []}
-                onChange={
-                  (e) => updateParameter(componentID, parameter_type, index, { options: e })
-                }
-              />
-            </Popover.Dropdown>
-          </Popover>
-        </>
-      );
-      break;
-    case 'datasheet':
-      additional_controls.push(
-        <>
-          <Popover
-            key='datasheet-editor'
-            closeOnClickOutside={false}
-            trapFocus
-            width={500}
-            position="bottom"
-            withArrow
-            shadow="md"
-          >
-            <Popover.Target>
-              <Button size="xs" variant="outline">
-                Select Datasheet Options
-              </Button>
-            </Popover.Target>
-            <Popover.Dropdown>TO DO</Popover.Dropdown>
-          </Popover>
-        </>
-      );
-      break;
-    default:
-      break;
+  function AdditionalControls() {
+    switch (data.data_type) {
+      case 'number':
+        return (
+          <>
+            <TextInput
+              key={`number-default-value-editor${index}`}
+              size="xs"
+              value={data.default_value}
+              onChange={(e) =>
+                updateParameter(componentID, parameter_type, index, {
+                  default_value: e.target.value,
+                })
+              }
+              withAsterisk
+              w={100}
+              description="Default Value"
+            />
+            <Select
+              key={`select-default-unit-editor${index}`}
+              w={60}
+              description="units"
+              size="xs"
+              value={data.default_unit}
+              onChange={(e) =>
+                updateParameter(componentID, parameter_type, index, { default_unit: e })}
+              data={['mm']}
+            />
+          </>
+        );
+      case 'table':
+        return (
+          <>
+            <Popover
+              key="table-editor"
+              closeOnClickOutside={false}
+              trapFocus
+              width={500}
+              position="bottom"
+              withArrow
+              shadow="md"
+            >
+              <Popover.Target>
+                <Button size="xs" variant="outline">
+                  Edit Table Data
+                </Button>
+              </Popover.Target>
+              <Popover.Dropdown>
+                <SvelteJSONEditor
+                  mode="table"
+                  content={{ json: data.table }}
+                  onChange={(e) =>
+                    updateParameter(componentID, parameter_type, index, { table: e.json })
+                  }
+                />
+                <Center>
+                  <DataPlotter title={data.label} data={data.table} />
+                </Center>
+              </Popover.Dropdown>
+            </Popover>
+          </>
+        );
+      case 'string':
+        break;
+      case 'boolean':
+        break;
+      case 'option':
+        return (
+          <>
+            <Popover
+              trapFocus
+              width={500}
+              position="bottom"
+              withArrow
+              shadow="md"
+            >
+              <Popover.Target>
+                <Button size="xs" variant="outline">
+                  Edit Options
+                </Button>
+              </Popover.Target>
+              <Popover.Dropdown>
+                <TagsInput
+                  variant="filled"
+                  size="xs"
+                  description="options list"
+                  value={data.options || []}
+                  onChange={
+                    (e) => updateParameter(componentID, parameter_type, index, { options: e })
+                  }
+                />
+              </Popover.Dropdown>
+            </Popover>
+          </>
+        );
+      case 'datasheet':
+        return (
+          <>
+            <Popover
+              key='datasheet-editor'
+              closeOnClickOutside={false}
+              trapFocus
+              width={500}
+              position="bottom"
+              withArrow
+              shadow="md"
+            >
+              <Popover.Target>
+                <Button size="xs" variant="outline">
+                  Select Datasheet Options
+                </Button>
+              </Popover.Target>
+              <Popover.Dropdown>TO DO</Popover.Dropdown>
+            </Popover>
+          </>
+        );
+      default:
+        break;
+    }
   }
 
   return (
@@ -195,7 +193,7 @@ export function Breakout(props) {
             />
           </>
         )}
-        {additional_controls}
+        <AdditionalControls />
       </Flex>
       <Tooltip label={JSON.stringify({ componentID, index, parameter_type })}>
         <CloseButton
